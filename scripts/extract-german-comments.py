dbb2c159cf54706acf506dcfa67eb8526d8a36c0
Philipp Heinrich <philipp.heinrich@fau.de>
2022-04-25 20:03:24 +0200

    switch to fasttext for language classification

--- scripts/extract-german-comments.py
+++ scripts/extract-german-comments.py
@@ -1,56 +1,42 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# based on a script available from https://github.com/adbar/german-reddit
-# original copyright Adrien Barbaresi, 2015.
-# MIT license
-
-# modifications by Philipp Heinrich, 2022.
+# Philipp Heinrich, 2022.
 
 import argparse
 import gzip
 import re
 from glob import glob
 
-import langid                   # pip3 install langid
 import ujson                    # pip3 install ujson
+import fasttext                 # pip3 install fasttext
 
-from utils import multi_proc, path2lines, spell_check
+from utils import multi_proc, path2lines
 
 
-# line-by-line filtering
-def process_line(line):
-
-    # load line
-    parsed_json = ujson.loads(line.strip())
+def process_line(comment):
 
     # sanitize text
-    sanitized_body = parsed_json['body'].replace('\r', '')
-    sanitized_body = sanitized_body.replace('\n', ' ')
-    sanitized_body = re.sub(r'\(?http[^ ]+\)?', '', sanitized_body)
+    body = comment['body'].replace('\r', '').replace('\n', ' ')
+    body = re.sub(r'\(?http[^ ]+\)?', '', body)
 
-    # don't categorize very short or deleted texts
-    if len(sanitized_body) <= 10 or sanitized_body == '[deleted]':
-        german = None
+    # don't analyze very short or deleted texts
+    if body == '[deleted]' or body == '[removed]':
+        lang = ""
+        confidence = ""
+        length = ""
 
+    # analysis
     else:
-        german = False
-
-        # first test: spell-checking
-        potentially_german = spell_check(sanitized_body)
-
-        # second test: language classification
-        if potentially_german:
-            langid_response = langid.classify(sanitized_body)
-            if langid_response[0] == 'de':
-                german = True
+        result = MODEL.predict(body)
+        lang = result[0][0]
+        confidence = str(result[1][0])
+        length = str(len(body))
 
     return {
-        'idx': parsed_json['id'],
-        # 'txt': sanitized_body,
-        'line': line,
-        'subreddit': parsed_json['subreddit'],
-        'german': german
+        'language': lang,
+        'confidence': confidence,
+        'length': length
     }
 
 
@@ -59,19 +45,41 @@ def process_file(path_in):
     # identify compression
     compression = path_in.split(".")[-1]
 
-    # set paths out
-    path_german = path_in.replace("." + compression, "-de.ldjson.gz")
+    # set path to output
     path_meta = path_in.replace("." + compression, "-lang.tsv.gz")
 
     # do the actual processing
-    with gzip.open(path_german, "wt") as f_german, gzip.open(path_meta, "wt") as f_meta:
+    with gzip.open(path_meta, "wt") as f_meta:
+
+        # header
+        f_meta.write("\t".join([
+            'comment_id',
+            'created_utc',
+            'link_id',         # thread
+            'parent_id',       # actual parent (submission or comment)
+            'subreddit',
+            'subreddit_id',
+            'language',
+            'confidence',
+            'length'
+        ]) + "\n")
+
         for line in path2lines(path_in):
-            result = process_line(line)
-            f_meta.write("\t".join(
-                [result['idx'], result['subreddit'], str(result['german'])]
-            ) + "\n")
-            if result['german']:
-                f_german.write(result['line'] + "\n")
+
+            comment = ujson.loads(line.strip())
+            analysis = process_line(comment)
+
+            f_meta.write("\t".join([
+                comment['id'],
+                str(comment['created_utc']),
+                comment['link_id'],
+                comment['parent_id'],
+                comment['subreddit'],
+                comment['subreddit_id'],
+                analysis['language'],
+                analysis['confidence'],
+                analysis['length']
+            ]) + "\n")
 
 
 def main():
@@ -81,14 +89,23 @@ def main():
     parser.add_argument('inputglob',
                         type=str,
                         help='glob to input files')
+    parser.add_argument('--lang_model',
+                        type=str,
+                        help='path to language model used by fasttext (.bin)',
+                        default="/home/ausgerechnet/corpora/models/lid.176.bin")
     parser.add_argument('--nr_proc',
                         type=int,
                         default=2,
                         help='how many processes to spawn')
     args = parser.parse_args()
 
-    # set paths
+    # glob input paths
     paths_in = glob(args.inputglob)
+
+    # load language model
+    path_model = args.lang_model
+    global MODEL
+    MODEL = fasttext.load_model(path_model)
 
     # process files
     multi_proc(process_file, paths_in, nr_cpus=args.nr_proc)
