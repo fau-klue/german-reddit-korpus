9ff21f716db00f33d79e2d8da74517d1fec68a02
Philipp Heinrich <philipp.heinrich@fau.de>
2022-05-07 12:29:45 +0200

    script for aggregating monthly german props

---
+++ scripts/prop-german-complete.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+
+# Philipp Heinrich, 2022
+
+suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(tidyverse))
+
+parser <- ArgumentParser()
+parser$add_argument("glob_in",
+                    help = "glob to input files")
+parser$add_argument("path_out",
+                    help = "path to output")
+args <- parser$parse_args()
+
+modus <- function(x) {
+  # use the mode for subreddit of thread, since threads can span over different subreddits (?)
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+paths <- Sys.glob(args$glob_in)
+
+print(str_c("collected ", as.character(length(paths)), " files"))
+
+d <- tibble()
+for (p in paths){
+  print(p)
+  d <- rbind(d, read_tsv(p))
+}
+
+var <- colnames(d)[1]
+
+print(str_c("grouping by: ", var))
+
+if (var == "subreddit"){
+  d <- d %>% group_by(!!sym(var)) %>%
+    summarise(confidence = weighted.mean(confidence, length), 
+              length = sum(length), 
+              n = sum(n)) %>%
+    arrange(desc(confidence)) %>%
+    replace_na(list(confidence = 0)) %>%
+    write_tsv(args$path_out)
+} else if (var == "link_id"){
+  d <- d %>% group_by(!!sym(var)) %>%
+    summarise(subreddit = modus(subreddit),
+              confidence = weighted.mean(confidence, length), 
+              length = sum(length), 
+              n = sum(n)) %>%
+    arrange(desc(confidence)) %>%
+    replace_na(list(confidence = 0)) %>%
+    write_tsv(args$path_out)
+} else {
+  print("wrong grouping variable")
+}
